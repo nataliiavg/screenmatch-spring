64366a243847a3b71bd836469d01dbaba338ebf2
Natalia Graciano <nataliavgraciano@gmail.com>
2024-07-08 12:03:59 -0300

    Add package services com ConsumoApi

---
+++ src/main/java/br/com/alura/screenmatch/ScreenmatchApplication.java
@@ -0,0 +1,24 @@
+package br.com.alura.screenmatch;
+
+import br.com.alura.screenmatch.service.ConsumoApi;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenmatchApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenmatchApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		//var inferência de tipo
+		var consumoApi = new ConsumoApi();
+		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&Season=1&apikey=b36f4f4e");
+		System.out.println(json);
+		json = consumoApi.obterDados("https://coffee.alexflipnote.dev/random.json");
+		System.out.println(json);
+	}
+}
