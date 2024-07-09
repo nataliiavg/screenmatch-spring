2fc3a2a532eecba5632e1f742b5d07ad97232b54
Natalia Graciano <nataliavgraciano@gmail.com>
2024-07-09 18:54:13 -0300

    Create classes Principal.java, DadosTemporada.java, DadosEpisodio e change em ScreenmatchApplication.java

---
+++ src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -0,0 +1,20 @@
+package br.com.alura.screenmatch.principal;
+
+import br.com.alura.screenmatch.service.ConsumoApi;
+
+import java.util.Scanner;
+
+public class Principal {
+    private Scanner leitura = new Scanner(System.in);
+    private ConsumoApi consumo = new ConsumoApi();
+
+    private final String ENDERECO = "https://www.omdbapi.com/?t=";
+    private final String API_KEY = "&apikey=b36f4f4e";
+
+    public void exibeMenu() {
+        System.out.println("Digite o nome da série para a busca: ");
+        var nomeSerie = leitura.nextLine();
+        var json = consumo.obterDados(ENDERECO + nomeSerie.replace(" ", "+") + API_KEY);
+
+    }
+}
